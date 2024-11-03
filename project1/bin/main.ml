569506da79b46124aee386c5ffea50f9ba43f88f
Connor George <georgecs1@sunypoly.edu>
2024-11-03 13:41:50 -0500

    got some working html to work as functions for creating, updating and deleting tasks

--- project1/bin/main.ml
+++
@@ -1,9 +0,0 @@
-open Opium
-
-let hello_handler _req =
-  Response.of_plain_text "Hello, World!" 
-    |> Lwt.return
-let () = 
-  App.empty
-    |> App.get "/" hello_handler
-    |> App.run_command
